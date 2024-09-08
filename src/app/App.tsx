2cb44e47c537018bfe0b9310de2565a7be674826
Fa-d <fsadakathussain@gmail.com>
2024-09-08 16:08:13 +0600

    skills section completed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,6 +8,8 @@ import Projects from '../components/projects/Projects.tsx';
 import { allProjects } from '../data/ProjectData.tsx';
 import ArticleNote from '../components/articles/ArticleNote.tsx';
 import { articleData } from '../data/ArticleNote.tsx';
+import Skills from '../components/skills/Skills.tsx';
+import { skillsList } from '../data/SkillsData.tsx';
 
 
 function App() {
@@ -16,12 +18,12 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/skills" element={<Main />} />
+          <Route path="/skills" element={<Skills items={skillsList} />} />
           <Route path="/" element={<Main />} />
           <Route path="/articles" element={<ArticleNote items={articleData} isArticle={true} />} />
-          <Route path="/projects" element={ <Projects items={allProjects} />} />
+          <Route path="/projects" element={<Projects items={allProjects} />} />
         </Routes>
-        <Footer />   
+        <Footer />
       </BrowserRouter>
     </>
 
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -7,7 +7,9 @@ import ReactGA from 'react-ga4';
 import Projects from '../projects/Projects'
 import { allProjects } from '../../data/ProjectData'
 import ArticleNote from '../articles/ArticleNote'
-import {articleData, noteData} from '../../data/ArticleNote'
+import { articleData, noteData } from '../../data/ArticleNote'
+import Skills from '../skills/Skills'
+import { skillsList } from '../../data/SkillsData'
 
 
 export default function Main() {
@@ -15,7 +17,9 @@ export default function Main() {
 
   return (
     <>
+      
       <About />
+      <Skills items={skillsList} />
       <CareerSteps steps={careerStepsData} />
       <Education steps={educationStepsData} />
       <Projects items={allProjects} />
